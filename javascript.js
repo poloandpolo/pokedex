18b21bc5980d15cf4bf3ef61d4fdb5ee2b89c0df
poloandpolo <158531147+poloandpolo@users.noreply.github.com>
2024-06-10 16:20:07 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,73 +1,66 @@
-const contenedor = document.getElementById("mainContainer")
-const searchingBar = document.getElementById("searchingBar")
-
+const contenedor = document.getElementById("mainContainer");
+const searchingBar = document.getElementById("searchingBar");
 
 const getPokemons = async (id) => {
-
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-
-    const data = await response.json()
-
-    return (data)
-}
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    return data;
+};
 
 const writePokemons = async (numberPokemons) => {
-    
+    const pokemonPromises = [];
     for (let i = 1; i <= numberPokemons; i++) {
-
-        const tempPokemon = (await getPokemons(i))
-
-        if ((Boolean(tempPokemon.types[1]))){
-            contenedor.innerHTML += `<div class="pokecard" pokemon-name=${tempPokemon.name} pokemon-id=${tempPokemon.id}>
-            <img src="${tempPokemon.sprites.front_default}" alt="">
-                <p>${tempPokemon.name}</p>
-                <p>Type: "${tempPokemon.types[0].type.name}, ${tempPokemon.types[1].type.name}"</p>
-        </div>`
-        }else{
-            contenedor.innerHTML += `<div class="pokecard" pokemon-name=${tempPokemon.name} pokemon-id=${tempPokemon.id}>
-            <img src="${tempPokemon.sprites.front_default}" alt="">
-                <p>${tempPokemon.name}</p>
-                <p>Type: "${tempPokemon.types[0].type.name}"</p>
-        </div>`
+        pokemonPromises.push(getPokemons(i));
     }
-    }
-}
 
-writePokemons(200).then(() => {
-    const data = Array.from(document.querySelectorAll(".pokecard"))
+    const allPokemons = await Promise.all(pokemonPromises);
 
-    console.log(data)
+    let html = '';
+    allPokemons.forEach((tempPokemon) => {
+        if (tempPokemon.types[1]) {
+            html += `<div class="pokecard" pokemon-name="${tempPokemon.name}" pokemon-id="${tempPokemon.id}">
+                        <img src="${tempPokemon.sprites.front_default}" alt="">
+                        <p>${tempPokemon.name}</p>
+                        <p>Type: ${tempPokemon.types[0].type.name}, ${tempPokemon.types[1].type.name}</p>
+                     </div>`;
+        } else {
+            html += `<div class="pokecard" pokemon-name="${tempPokemon.name}" pokemon-id="${tempPokemon.id}">
+                        <img src="${tempPokemon.sprites.front_default}" alt="">
+                        <p>${tempPokemon.name}</p>
+                        <p>Type: ${tempPokemon.types[0].type.name}</p>
+                     </div>`;
+        }
+    });
 
-    searchingBar.addEventListener("keyup", async (e) => {
+    contenedor.innerHTML = html;
+};
 
-        if (searchingBar.value == ""){
-            let tempHtml = ""
+writePokemons(1025).then(() => {
+    const data = Array.from(document.querySelectorAll(".pokecard"));
 
+    searchingBar.addEventListener("keyup", () => {
+        let tempHtml = "";
+        const inputValue = searchingBar.value.toLowerCase();
+
+        if (inputValue === "") {
             data.forEach((pokecard) => {
-                tempHtml += `<div class="pokecard">${pokecard.innerHTML}</div>`
-            })
-
-            contenedor.innerHTML = tempHtml
-
-        }else{
-            
-            let tempHtml = "";
-            const inputValue = searchingBar.value.toLowerCase(); 
-        
-            const filteredPokemon = data.filter((pokemon) => {
-                const pokemonName = pokemon.getAttribute("pokemon-name").toLowerCase(); 
-                return pokemonName.includes(inputValue); 
+                tempHtml += `<div class="pokecard">${pokecard.innerHTML}</div>`;
             });
-        
+        } else {
+            const filteredPokemon = data.filter((pokemon) => {
+                const pokemonName = pokemon.getAttribute("pokemon-name").toLowerCase();
+                return pokemonName.includes(inputValue);
+            });
+
             filteredPokemon.forEach((pokemon) => {
                 tempHtml += `<div class="pokecard">${pokemon.innerHTML}</div>`;
             });
-        
-            contenedor.innerHTML = tempHtml;
         }
-    
-})
-})
+
+        contenedor.innerHTML = tempHtml;
+    });
+});
+
 
 
 
